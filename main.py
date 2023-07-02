2dffaade4430a1f8b9af71a06a8433e73a0b45e1
Bhavya Jain <contact.jain.bhavya@gmail.com>
2023-07-02 10:27:40 +0530

    Test

--- main.py
+++ main.py
@@ -3,5 +3,31 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def basics(function_to_run, input_set):
+
+    match function_to_run:
+        case "printing":
+            return print(input_set)
+
+        case "addition":
+            return print(input_set[0] + input_set[1])
+
+        case "str_to_int":
+            try:
+                return print(int(input_set))
+            except:
+                print("Incorrect value")
+                quit()
+
+        case "max":
+            print(function_to_run)
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # basics("printing", ["testing", "tester"])
+    # basics("addition", [5, 1])
+    # basics("str_to_int", 0)
+
+    function_name = input('Tell me you want string to int ')
+    basics(function_name, "one")
